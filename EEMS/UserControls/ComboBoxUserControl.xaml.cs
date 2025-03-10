69a10ec8041919391b0a68cc1d773ad351d05c4b
MohammedChami <mohamuza@gmail.com>
2025-03-10 18:58:39 +0100

    feat: Enhance UI components and data binding
    - Added `FamilySituation` property in
      `AddAndEditEmployeeViewModel.cs` with initial values.
    - Created new `ComboBoxUserControl` with binding for
      `Items` and `Caption`.
    - Added `JobInformationUserControl.xaml` and its code-behind.
    - Updated `PersonalInformationUserControl.xaml` to include
      `ComboBoxUserControl` for family situations.
    - Enhanced `PersonalInformationUserControl.xaml.cs` with
      properties for data binding.

---
+++ EEMS/UserControls/ComboBoxUserControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace EEMS.UI.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ComboBoxUserControl.xaml
+    /// </summary>
+    public partial class ComboBoxUserControl : UserControl
+    {
+        public ComboBoxUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public string Caption
+        {
+            get { return (string)GetValue(CaptionProperty); }
+            set { SetValue(CaptionProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Caption.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CaptionProperty =
+            DependencyProperty.Register("Caption", typeof(string), typeof(ComboBoxUserControl), new PropertyMetadata(string.Empty));
+
+        public IEnumerable<string> Items
+        {
+            get { return (IEnumerable<string>)GetValue(ItemsProperty); }
+            set { SetValue(ItemsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Items.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ItemsProperty =
+            DependencyProperty.Register("Items", typeof(IEnumerable<string>), typeof(ComboBoxUserControl), new PropertyMetadata(new List<string>()));
+
+
+
+
+
+    }
+
+
+}
